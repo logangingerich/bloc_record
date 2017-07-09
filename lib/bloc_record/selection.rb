7aa2af5b8e5cc27ca58c0e998734ded356c1768f
Logan Gingerich <logangingerich@yahoo.com>
2017-07-09 14:10:55 -0600

    Checkpoint 5: Complete

--- lib/bloc_record/selection.rb
+++ lib/bloc_record/selection.rb
@@ -198,7 +198,9 @@ module Selection
   end
 
   def rows_to_array(rows)
-    rows.map { |row| new(Hash[columns.zip(row)]) }
+    collection = BlocRecord::Collection.new
+    rows.each { |row| collection << new(Hash[columns.zip(row)]) }
+    collection
   end
 
   def method_missing(m, *args, &block)
